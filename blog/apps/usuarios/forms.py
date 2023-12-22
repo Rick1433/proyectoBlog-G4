df965f5ec10eda88ab7daaf33dd4cf545cda67da
Ricardo <alvarerd1993@gmail.com>
2023-12-22 16:04:57 -0300

    final

--- blog/apps/usuarios/forms.py
+++ blog/apps/usuarios/forms.py
@@ -3,14 +3,23 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
 class FormularioRegistro(UserCreationForm):
-    email = forms.EmailField()
+    username = forms.CharField(
+        label="Nombre de Usuario",
+        widget=forms.TextInput(attrs={'class': 'form-control-registro'}),
+    )
+    email = forms.EmailField(widget=forms.TextInput(attrs={'class': 'form-control-registro'}))
     password1 = forms.CharField(
         label="Contraseña",
-        widget=forms.PasswordInput,
+        widget=forms.PasswordInput(attrs={'class': 'form-control-registro'}),
     )
     password2 = forms.CharField(
-        label="Confirmar Contraseña", widget=forms.PasswordInput
+        label="Confirmar Contraseña", widget=forms.PasswordInput(attrs={'class': 'form-control-registro'})
     )
 
     class Meta:
